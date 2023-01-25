1ce37614234ea045150e8f558a3972f978f662a1
taboterm <taboterm@student.42wolfsburg.de>
2023-01-25 22:03:29 +0100

    load images to map functions - wip

--- load_images.c
+++ load_images.c
@@ -6,29 +6,27 @@
 /*   By: taboterm <taboterm@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 18:14:26 by taboterm          #+#    #+#             */
-/*   Updated: 2023/01/22 21:45:17 by taboterm         ###   ########.fr       */
+/*   Updated: 2023/01/25 21:48:01 by taboterm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"so_long.h"
 
-void	load_images(t_game *game)
+void	load_fl(t_game *game)
 {
 	game->tile.fl = (t_tile *)malloc(sizeof(t_tile));
-
-	game->tile.fl = mlx_xpm_file_to_image(game->mlx, ".images/floor_100x100.xpm", \
+	game->tile.fl = mlx_xpm_file_to_image(game->mlx, FLOOR_TILE, \
 	&game->tile.w, &game->tile.h);
 	if (!game->tile.fl)
 		image_fail(game);
-	
 }
 
-void	img_to_win(t_game *game, void *img)
+void	add_tiles(t_game *game)
 {
-	int	i;
-	int j;
-	
-	i = 0;
-	j = 0;
+	load_fl(game);
+}
+
+void	img_to_map(t_game *game, void *img, int i, int j)
+{
 	mlx_put_image_to_window(game->mlx, game->win, img, j * 100, i * 100);
 }
