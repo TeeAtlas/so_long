cb9620ee97705657a51d34435f163e2c6f30312b
taboterm <taboterm@student.42wolfsburg.de>
2023-02-03 15:55:34 +0100

    put image to window

--- display_tile.c
+++ display_tile.c
@@ -6,30 +6,29 @@
 /*   By: taboterm <taboterm@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 18:14:26 by taboterm          #+#    #+#             */
-/*   Updated: 2023/02/01 15:01:52 by taboterm         ###   ########.fr       */
+/*   Updated: 2023/02/03 15:45:21 by taboterm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"so_long.h"
 
-void	image_to_map(t_game *game)
+/* xpm files to pixels */
+void	image_xpm_to_map(t_game *game)
 {
-	int h;
-	int w;
+	game->tile.fl = mlx_xpm_file_to_img(game->mlx, FLOOR,\
+									&game->map.w, &game->map.h);
+}
 
-	h = 0;
-	game->h = game->map.h;
-	game->w = game->map.w;
-	while (h < game->h)
-	{
-		w = 0;
-		while (w < game->w)
-		{
-			if (game->map.str_2[h * game->w + w] == '1')
-				mlx_xpm_file_to_image(game->mlx,\
-	 			"images/floor_100x100.xpm", &game->tile.tile_dimension, &game->tile.tile_dimension);
-			w++;
-		}
-		h++;
-	}
+/* images to the map */
+void	load_image(t_game *game, int i, int j)
+{
+	size_t	x;
+	size_t	y;
+
+	x = i * TILE;
+	y = j * TILE;
+	if (game->map.map_file[i][j] =='1')
+		mlx_put_image_to_window(game->mlx, game->win, \
+		game->tile.fl, x, y);
+	
 }
---
+++ readmap.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   readmap.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taboterm <taboterm@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/20 15:44:23 by taboterm          #+#    #+#             */
+/*   Updated: 2023/02/03 15:09:55 by taboterm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	malloc_maps(t_game *game)
+{
+	game->map.map_copy = malloc (sizeof(char *) * (game->map.rows + 1));
+	if (!game->map.map_copy)
+		exit(EXIT_SUCCESS);
+}
+
+void	read_map_array(t_game *game)
+{
+	game->fd = open(game->map.map_file, O_RDONLY);
+	game->map.str = malloc (sizeof(char));
+	game->map.str_2 = malloc (sizeof(char));
+	while (game->map.str)
+	{
+		game->map.str = get_next_line(game->fd);
+		if(game->map.str)
+			game->map.str_2 = ft_strjoin(game->map.str_2, game->map.str);
+	}
+	printf("map str: \n%s\n", game->map.str_2);
+}
+
+//do I need to strjoin the array?
+
+//counts columns and rows ie: height(row) and width(col)
+void	read_map(t_game *game)
+{
+	char	*line;
+	
+	game->map.rows = 0;
+	game->fd = open(game->map.map_file, O_RDONLY);
+	line =  get_next_line(game->fd);
+	if (!line)
+	{
+		ft_printf("Error: No file to read");
+		exit (EXIT_SUCCESS);
+	}
+	game->map.w = maplinelen(line) - 1;
+	while (line != 0)
+	{
+		free(line);
+		line = get_next_line(game->fd);
+		game->map.rows++;
+	}
+	game->map.h = game->map.rows;
+	printf("game w:%d\n", game->map.w);
+	close(game->fd);
+	malloc_maps(game);
+}
+
+
+
