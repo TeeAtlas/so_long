9f5755b155fdaeeb51dcccef274fd2512c5f4c36
taboterm <taboterm@student.42wolfsburg.de>
2023-02-11 12:05:45 +0100

    working function to place tiles in window using mlx

--- display_tile.c
+++ display_tile.c
@@ -6,20 +6,12 @@
 /*   By: taboterm <taboterm@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 18:14:26 by taboterm          #+#    #+#             */
-/*   Updated: 2023/02/10 18:39:43 by taboterm         ###   ########.fr       */
+/*   Updated: 2023/02/11 12:00:26 by taboterm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"so_long.h"
 
-// /* xpm files to pixels */
-// void	image_xpm_to_map(t_game *game)
-// {
-// 	game->tile.fl = mlx_xpm_file_to_image(game->mlx, FLOOR, \
-// 									&game->map.w, &game->map.h);
-// }
-
-/* images to the map */
 void	load_image(t_game *game)
 {
 	int i;
@@ -29,18 +21,24 @@ void	load_image(t_game *game)
 	
 	i = 0;
 	j = 0;
-	x = 0;
-	y = 0;
-	x = i * TILE;
-	y = j * TILE;
 	game->tile.fl = NULL;
 	game->tile.fl = mlx_xpm_file_to_image(game->mlx, "./images/floor_100x100.xpm", \
 									&x, &y);
-	ft_printf("HERE3 w: %i h: %i img pointer %p\n", x, y, game->tile.fl);
-	if (game->map.array[j][i] == '1')
+	while (game->map.array[j])
 	{
-		ft_printf("In if w: %i h: %i \n", game->map.w, game->map.h);
-		mlx_put_image_to_window(game->mlx, game->win, game->tile.fl, 0, 0);
-	}
-	
+		while (game->map.array[j][i])
+		{
+			if (game->map.array[j][i] == '0')
+				{
+					mlx_put_image_to_window(game->mlx, game->win, game->tile.fl, i * 100, j * 100);
+				}	
+			// else if (game->map.array[j][i] == 'C')
+			// 	{
+			// 		mlx_put_image_to_window(game->mlx, game->win, game->tile.ch, i * 100, j * 100);
+			// 	}
+			i++;
+		}
+		i = 0;
+		j++;
+	}			
 }
