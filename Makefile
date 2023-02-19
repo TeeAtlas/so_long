21e26ea41c592b77fac70c57aba37910c943dbed
taboterm <taboterm@student.42wolfsburg.de>
2023-02-19 10:23:50 +0100

    no changes

--- Makefile
+++ Makefile
@@ -6,13 +6,13 @@
 #    By: taboterm <taboterm@student.42wolfsburg.    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/12/19 15:17:12 by taboterm          #+#    #+#              #
-#    Updated: 2023/02/13 09:37:47 by taboterm         ###   ########.fr        #
+#    Updated: 2023/02/16 18:57:01 by taboterm         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 # source id
 SRCS = so_long.c map_check.c freeing.c\
-	readmap.c load_xpm.c display_tile.c\
+	readmap.c display_tile.c\
 
 
 OBJS = $(SRCS:%.c=%.o)
--- display_tile.c
+++ display_tile.c
@@ -6,11 +6,26 @@
 /*   By: taboterm <taboterm@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 18:14:26 by taboterm          #+#    #+#             */
-/*   Updated: 2023/02/16 11:46:16 by taboterm         ###   ########.fr       */
+/*   Updated: 2023/02/16 18:56:34 by taboterm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"so_long.h"
+
+void	xpm_to_pixel(t_game *game)
+{
+	game->tile.fl = NULL;
+	game->tile.ex = NULL;
+	game->tile.pl = NULL;
+	game->tile.wl = NULL;
+	game->tile.cl = NULL;
+	
+	game->tile.fl = mlx_xpm_file_to_image(game->mlx, FLOOR, &game->x, &game->y);
+	game->tile.ex = mlx_xpm_file_to_image(game->mlx, EXIT, &game->x, &game->y);
+	game->tile.pl = mlx_xpm_file_to_image(game->mlx, PLAY, &game->x, &game->y);
+	game->tile.wl = mlx_xpm_file_to_image(game->mlx, WALL, &game->x, &game->y);
+	game->tile.cl = mlx_xpm_file_to_image(game->mlx, COLL, &game->x, &game->y);
+}
 
 // loads image to window - defined by tile size etc and map co-ordinates
 void	load_image(t_game *game)
