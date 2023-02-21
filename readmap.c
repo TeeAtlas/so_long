9386ede2eff168de4218397d5a30e989f6e61489
Tania Boterman <taboterm@c2s4d9.42wolfsburg.de>
2023-02-21 17:27:57 +0100

    adding walled in check

--- readmap.c
+++ readmap.c
@@ -6,7 +6,7 @@
 /*   By: taboterm <taboterm@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 15:44:23 by taboterm          #+#    #+#             */
-/*   Updated: 2023/02/21 14:43:31 by taboterm         ###   ########.fr       */
+/*   Updated: 2023/02/21 17:27:13 by taboterm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,34 @@
 //counts columns and rows ie: height(row) and width(col)
 // exit (0) = EXIT_SUCCESS
 // exit (1) = EXIT_FAILURE
+
+int	walled_t_b(t_game *game)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = game->map.h - 1;
+	while (game->map.array[i])
+	{
+		while (i < game->map.w)
+		{
+			if (game->map.array[0][i] != '1')
+			{
+				ft_printf("Error: Not walled in\n");
+				exit ;
+			}
+			if (game->map.array[j][i] != '1')
+			{
+				ft_printf("Error: Not walled in\n");
+				exit ;
+			}
+			i++;
+		}
+		return (0);
+	}
+	return (1);
+}
 
 void	find_player(t_game *game)
 {
@@ -65,28 +93,3 @@ void	read_map(t_game *game)
 	game->map.array = ft_split(game->map.str_line, '\n');
 	close(game->fd);
 }
-
-// void	walled_in(t_game *game)
-// {
-	
-// }
-
-// void	read_map_line(t_game *game)
-// {
-// 	char	*line;
-// 	char	*temp;
-	
-// 	game->fd = open(game->map.map_file, O_RDONLY);
-// 	line = get_next_line(game->fd);
-// 	while (line)
-// 	{
-// 		valid_dimensions(line);
-// 		temp = ft_strjoin(game->map.str_line, line);
-// 		free(game->map.str_line);
-// 		game->map.str_line = temp;
-// 		free(line);
-// 		line = get_next_line(game->fd);
-// 	}
-// 	ft_printf("line:%s\n", game->map.line);
-// 	close(game->fd);
-// }
