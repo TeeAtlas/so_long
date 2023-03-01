0b4cc705b5c4adca86c01cf7a70a446ee7d6cfca
taboterm <taboterm@student.42wolfsburg.de>
2023-03-01 20:31:57 +0100

    print 2d array

---
+++ printsplit.c
@@ -0,0 +1,32 @@
+// #include "so_long.h"
+
+// void	printsplit(char **split)
+// {
+// 	t_game *game;
+//     int    i;
+//     int    j;
+
+// 	game = NULL;
+//     i = 0;
+//     j = 0;
+// 	split = game->map.array;
+//     while (split[i])
+//     {
+//         j = 0;
+//         while (split[i][j])
+//         {
+//             ft_printf("%c", split[i][j]);
+//             j++;
+//         }
+//         printf("\n");
+//         i++;
+//     }
+// } 
+
+// int main(void)
+// {
+// 	t_game *game;
+	
+//  	printsplit(game->map.arra);
+//  	return(0);
+// }
