1afffdacbc81b049502e7051b2752b3ba7708332
taboterm <taboterm@student.42wolfsburg.de>
2023-03-11 12:25:12 +0100

    modified function

--- image_failure.c
+++ image_failure.c
@@ -6,7 +6,7 @@
 /*   By: taboterm <taboterm@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 21:09:11 by taboterm          #+#    #+#             */
-/*   Updated: 2023/01/22 22:03:36 by taboterm         ###   ########.fr       */
+/*   Updated: 2023/03/11 12:24:40 by taboterm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 void	image_fail(t_game *game)
 {
 	ft_printf("Error: Failed to load image\n");
-	free_num(game, 10);
-	mlx_destroy_window(game->mlx, game->win);
+	free(game);
 	exit(EXIT_FAILURE);
 }
